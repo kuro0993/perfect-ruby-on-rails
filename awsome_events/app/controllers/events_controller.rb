1260b89c0ba529db8ba510151373e891f4304de8
kuro0993 <kuro0993@gmail.com>
2023-03-21 16:29:42 +0900

    Create Event index page

--- awsome_events/app/controllers/events_controller.rb
+++ awsome_events/app/controllers/events_controller.rb
@@ -1,6 +1,10 @@
 class EventsController < ApplicationController
   skip_before_action :authenticate, only: :show
 
+  def index
+    @events = Event.where("start_at > ?", Time.zone.now).order(:start_at)
+  end
+
   def show
     @event = Event.find(params[:id])
   end
