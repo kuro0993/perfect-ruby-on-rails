65d2f7bd37e95a9de53053b7de25c1aaccebb599
kuro0993 <kuro0993@gmail.com>
2023-03-21 15:04:36 +0900

    Implemented Logout

--- awsome_events/config/routes.rb
+++ awsome_events/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # get 'welcome/index'
   root 'welcome#index'
   get '/auth/:provider/callback' => 'session#create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  delete '/logout' => 'session#destroy'
 end
