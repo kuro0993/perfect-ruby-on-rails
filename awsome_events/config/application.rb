c6cdcd1ea793eee59a0e4c2a18094d9eefe798db
kuro0993 <kuro0993@gmail.com>
2023-04-12 00:16:57 +0900

    Implement process to generate thumbnail

--- awsome_events/config/application.rb
+++ awsome_events/config/application.rb
@@ -25,10 +25,6 @@ module AwesomeEvents
     config.load_defaults 6.0
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.active_storage.variant_processor = :vips
   end
 end
